92403a6f294a9c66c2992b8e3c83572ae8296fac
CosminFRC <115585890+CosminFRC@users.noreply.github.com>
2024-05-07 22:27:38 +0300

    Update pool_monitor_card.js
    Added "Romanian". It's my first time, so if I didn't do it correct, sorry in advance.

--- pool_monitor_card.js
+++ pool_monitor_card.js
@@ -44,6 +44,37 @@ const translations = {
       "days": `{days} day{plural} ago`
     }
   },
+  'ro': {
+    "state": {
+      "1": "Prea mic",
+      "2": "Mic",
+      "3": "Ideal",
+      "4": "Ideal",
+      "5": "Mare",
+      "6": "Prea mare"
+    },
+    "sensor": {
+      "temperatură": "Temperatură",
+      "temperatură_2": "Temperatură 2",
+      "ph": "pH",
+      "orp": "ORP",
+      "tds": "TDS",
+      "salinity": "Salinitate",
+      "cya": "Acid cianuric",
+      "calcium": "Calciu",
+      "phosphate": "Fosfat",
+      "alkalinity": "Alcalinitate",
+      "free_chlorine": "Clor liber",
+      "total_chlorine": "Clor total",
+      "pressure": "Presiune filtru"
+    },
+    "time": {
+      "seconds": "acum",
+      "minutes": `acum {minutes} minut{plural}`,
+      "hours": `acum {hours} oră{plural}`,
+      "days": `acum {days} zi{plural}`
+    }
+  },
   'fr': {
     "state": {
       "1": "Trop bas",
